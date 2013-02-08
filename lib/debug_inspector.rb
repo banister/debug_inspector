a57550d1ebf3ccc06ee063528e0aa02524025c8b
John Mair <jrmair@gmail.com>
2013-02-08 19:39:43 +0100

    Perform a no-op if required on invalid ruby version
    We don't raise an exception, we just dont work :) This makes
    integrating with tools like binding_of_caller much easier.

--- lib/debug_inspector.rb
+++ lib/debug_inspector.rb
@@ -1,7 +1,5 @@
-require 'rbconfig'
-
-if File.exists?(File.expand_path(File.dirname(__FILE__), "debug_inspector.#{RbConfig::CONFIG["DLEXT"]}"))
+begin
   require 'debug_inspector.so'
-else
-  raise LoadError, "Cannot find debug_inspector.so: Are you sure you're running MRI 2.0?"
+rescue LoadError
 end
+
