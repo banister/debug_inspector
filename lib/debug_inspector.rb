11225e84df4542579d2f7bc6f813ca12f5752f41
John Mair <jrmair@gmail.com>
2013-02-08 19:07:17 +0100

    Ensure File.exists? looks in pwd

--- lib/debug_inspector.rb
+++ lib/debug_inspector.rb
@@ -1,4 +1,4 @@
-if File.exists?("debug_inspector.so")
+if File.exists?("./debug_inspector.so")
   require 'debug_inspector.so'
 else
   raise LoadError, "Cannot find debug_inspector.so: Are you sure you're running MRI 2.0?"
