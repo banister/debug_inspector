a3b09cb2b3276b4eb5aaf108a4604c8f098a0168
John Mair <jrmair@gmail.com>
2013-02-08 19:32:27 +0100

    improved Rakefile

--- lib/debug_inspector.rb
+++ lib/debug_inspector.rb
@@ -1,4 +1,6 @@
-if File.exists?("./debug_inspector.so")
+require 'rbconfig'
+
+if File.exists?(File.expand_path(File.dirname(__FILE__), "debug_inspector.#{RbConfig::CONFIG["DLEXT"]}"))
   require 'debug_inspector.so'
 else
   raise LoadError, "Cannot find debug_inspector.so: Are you sure you're running MRI 2.0?"
