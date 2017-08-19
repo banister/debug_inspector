421374df328dcc9737ecae111202be76fe3dbbe1
0xAB <0xAB@protonmail.com>
2017-08-20 00:57:42 +0100

    add tests that cover the basics

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -4,4 +4,22 @@ class BasicTest < MiniTest::Test
   def test_version
     assert(DebugInspector::VERSION)
   end
+
+  def test_backtrace_locations
+    RubyVM::DebugInspector.open do |dc|
+      assert dc.backtrace_locations.size > 0
+    end
+  end
+
+  def test_frame_binding
+    RubyVM::DebugInspector.open do |dc|
+      assert_equal self, dc.frame_binding(1).eval("self")
+    end
+  end
+
+  def test_frame_class
+    RubyVM::DebugInspector.open do |dc|
+      assert_equal self.class, dc.frame_class(1)
+    end
+  end
 end
