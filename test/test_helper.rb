bfcc8f40ddf6172269bb93573df15d1a941c8ab9
0xAB <0xAB@protonmail.com>
2017-08-20 19:18:10 +0100

    fallback to system environment if bundler is missing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
-require 'bundler/setup'
+begin
+  require 'bundler/setup'
+rescue LoadError
+  # Bundler is missing. This should happen rarely (eg while running tests from Fedora package).
+  # See https://github.com/banister/debug_inspector/pull/16 for more details.
+  $LOAD_PATH.push File.expand_path(File.join("..", "lib"), File.dirname(__FILE__))
+  warn "WARN: Bundler is not being used."
+end
+require 'minitest'
 require 'minitest/autorun'
-Bundler.require :default
+require 'debug_inspector'
