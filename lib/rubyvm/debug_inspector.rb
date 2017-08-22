1d43d95e282d424c75b9e6e9610b1fda90c44bec
0xAB <0xAB@protonmail.com>
2017-08-22 06:45:54 +0100

    dont define constants from Ruby if cant load C extension

--- lib/rubyvm/debug_inspector.rb
+++ lib/rubyvm/debug_inspector.rb
@@ -1,7 +1,8 @@
-require_relative "debug_inspector/version"
 require 'rbconfig'
 dlext = RbConfig::CONFIG['DLEXT']
 begin
   require "rubyvm/debug_inspector.#{dlext}"
+  # If the above require fails, we don't want to define any constants.
+  require_relative "debug_inspector/version"
 rescue LoadError
 end
