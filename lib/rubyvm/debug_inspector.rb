4eeea89c3f06dfece35054a925a9d89b4613dedd
Robin Daugherty <robin@robindaugherty.net>
2020-12-17 15:40:37 -0500

    Fix extension failing to load

--- lib/rubyvm/debug_inspector.rb
+++ lib/rubyvm/debug_inspector.rb
@@ -1,7 +1,7 @@
 require 'rbconfig'
 dlext = RbConfig::CONFIG['DLEXT']
 begin
-  require "rubyvm/debug_inspector.#{dlext}"
+  require_relative "../debug_inspector.#{dlext}"
   # If the above require fails, we don't want to define any constants.
   require_relative "debug_inspector/version"
 rescue LoadError
