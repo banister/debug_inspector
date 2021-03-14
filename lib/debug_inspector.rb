76b4060428dcf24a3b426c3253e96966508c5ab1
Robin Daugherty <robin@robindaugherty.net>
2021-03-14 19:40:30 -0400

    Try loading from both locations

--- lib/debug_inspector.rb
+++ lib/debug_inspector.rb
@@ -1,9 +1,17 @@
 require 'rbconfig'
 dlext = RbConfig::CONFIG['DLEXT']
 begin
-  require_relative "../ext/debug_inspector/debug_inspector.#{dlext}"
-  # If the above require fails, we don't want to define any constants.
+  # If the installation task did its job, the extension is in lib/ next to this file.
+  require "debug_inspector.#{dlext}"
+  # We only want to define constants if the extension has loaded.
   require_relative "rubyvm/debug_inspector/version"
-rescue LoadError => e
-  puts "debug_inspector extension was not loaded"
+rescue LoadError
+  begin
+    # If not, maybe the extension is in ext/
+    require_relative "../ext/debug_inspector/debug_inspector.#{dlext}"
+    # We only want to define constants if the extension has loaded.
+    require_relative "rubyvm/debug_inspector/version"
+  rescue LoadError => e
+    puts "debug_inspector extension was not loaded"
+  end
 end
